db0fdcf521b9432f92b8d12c31baf481d424fdad
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-06 14:42:01 +0000

    Generates the assembly code of a C code

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -3,9 +3,6 @@
 /**
  * main - Entry point
  *
- * Description: Main is a special type of function which I term a wrapper
- * function. It serves as the entry point for all C programs and it is executed
- * first.
  * Return: Always 0 (Success)
  */
 int main(void)
