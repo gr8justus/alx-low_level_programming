96d49db8d422b3fbb3628f5d191b0243a6a32e8f
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-06 23:43:14 +0000

    Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a_z = 97;
+
+	while (a_z < 123)
+	{
+		char lower_case_letter = (char)a_z;
+
+		putchar(lower_case_letter);
+		a_z++;
+	}
+	putchar('\n');
+	return (0);
+}
