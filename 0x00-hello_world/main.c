747456cd549d45479b1171a2d8743d2e24007ffd
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-06 14:35:15 +0000

    Compile a C file without linking

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
+ * Description: Main is a special type of function which I term a wrapper
+ * function. It serves as the entry point for all C programs and it is executed
+ * first.
  * Return: Always 0 (Success)
  */
 int main(void)
