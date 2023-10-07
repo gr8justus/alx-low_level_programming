e52ad5adc00c39c15b2bb2254e8eabe446cb8481
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-07 12:01:59 +0000

    Print the letters of the alphabet with the exception of "e" and "q"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a_z;
+	char lower_case_letter;
+
+	a_z = 97;
+
+	while (a_z < 123)
+	{
+		lower_case_letter = (char)a_z;
+
+		if (lower_case_letter == 'e' || lower_case_letter == 'q')
+			lower_case_letter = '\0';
+
+		putchar(lower_case_letter);
+		a_z++;
+	}
+	putchar('\n');
+	return (0);
+}
