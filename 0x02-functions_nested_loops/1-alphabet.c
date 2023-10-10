b37648bf131fc18dd1770dfe2e7e9e4aa7598108
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-10 20:46:41 +0000

    Print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Writes the alphabet to stdout
+ *
+ * Return: No return value
+ */
+void print_alphabet(void)
+{
+	int j = 97;
+
+	while (j < 123)
+	{
+		_putchar('0' + j);
+		j++;
+	}
+	 _putchar('\n');
+}
