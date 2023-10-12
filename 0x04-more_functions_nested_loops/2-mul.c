fad85d4474fa7783c109ad1aaae32bb20cb263b2
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-12 13:47:50 +0000

    Get the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Performs multiplication operation for two integers
+ * @a: Operand 1
+ * @b: Operand 2
+ *
+ * Return: Product of the two operands
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
