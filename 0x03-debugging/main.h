084074c2e26e327d385581486fdd6766551961b4
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-10-12 23:09:16 +0000

    Test function to give correct output

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int print-remaining_days(int month, int day,int year);
 
 #endif
