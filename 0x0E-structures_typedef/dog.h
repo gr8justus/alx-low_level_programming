38aee75477f19ee4e383d132c1632f3be50a1c6e
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-11-06 22:00:16 +0000

    Define struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog profiling
+ * @name: First data
+ * @age: Second data
+ * @owner: Third data
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
