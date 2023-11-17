ac7f9d27ec8c2ef6ef6a9a4c851b3b2535a66eb9
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-11-17 10:26:19 +0000

    Execute a function before main
    The function prints a string to stdout before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - executes before the main function.
+ *
+ * Return: Nothing.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
