d02f75a5e268bd01e577cda0fdc78c9de5684831
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-12-29 14:47:14 +0100

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum two integers
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: sum of integers
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
