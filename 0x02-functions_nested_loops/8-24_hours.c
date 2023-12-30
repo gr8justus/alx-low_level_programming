3cc0e181e06aa09251aff022d6d2fddebd61d409
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2023-12-30 12:39:58 +0100

    Update source code 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,30 +4,30 @@
  * jack_bauer - print every minute of the day
  *
  * Return: nothing
-*/
+ */
 void jack_bauer(void)
 {
-	int hour_digit_1, hour_digit_2, minutes, seconds, max = 9;
+	int hour_digit_1, hour_digit_2, minutes_digit_1, minutes_digit_2, max = 9;
 
 	for (hour_digit_1 = MIN; hour_digit_1 <= 2; hour_digit_1++)
 	{
 		for (hour_digit_2 = MIN; hour_digit_2 <= max; hour_digit_2++)
 		{
-			for (minutes = MIN; minutes <= 5; minutes++)
+			for (minutes_digit_1 = MIN; minutes_digit_1 <= 5; minutes_digit_1++)
 			{
-				for (seconds = MIN; seconds <= MAX; seconds++)
-				{
-					if (hour_digit_1 == 2)
-						max = 3;
+				if (hour_digit_1 == 2)
+					max = 3;
 
-					_putchar(hour_digit_1 | '0');
-					_putchar(hour_digit_2 | '0');
+				for (minutes_digit_2 = MIN; minutes_digit_2 <= MAX; minutes_digit_2++)
+				{
+					_putchar(hour_digit_1 + '0');
+					_putchar(hour_digit_2 + '0');
 					_putchar(':');
-					_putchar(minutes | '0');
-					_putchar(seconds | '0');
+					_putchar(minutes_digit_1 + '0');
+					_putchar(minutes_digit_2 + '0');
 					_putchar('\n');
-				} /* close seconds iteration block */
-			} /* close minutes iteration block */
+				} /* close minutes_digit_2 iteration block */
+			} /* close minutes_digit_1 iteration block */
 		} /* close hour_digit_2 iteration block */
 	} /* close hour_digit_1 iteration block */
 }
